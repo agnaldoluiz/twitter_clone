9a3bf5e4a11da7dadfe7fb802a0348743a68ffa6
Agnaldo Luiz <agnaldoluiz@gmail.com>
2014-09-15 16:28:57 -0500

    Twitter Clone Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'd52904d9aa63f7cf70525c50210e95931a19c15a15b7b5c36b28ca61aedcbc883b1c06537722590ed2261c0ebcc5d1c25a53e0cf16f3cbfb240eb3dd36f51d90'
